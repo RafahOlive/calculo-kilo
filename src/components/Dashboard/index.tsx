3a4bf2872871ddf917f2d92ecd43f56c63864be5
Rafael <rafaelribeiro12@live.com>
2022-03-27 10:24:20 -0300

    finally working on this idea!

---
+++ src/components/Dashboard/index.tsx
@@ -0,0 +1,6 @@
+export function Dashboard()
+{
+    return (
+        <h1>Site Content HERE!!!!!!!!</h1>
+    );
+}
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,6 @@
+export function Header()
+{
+    return (
+        <h1>Header Here!!</h1>
+    );
+}
