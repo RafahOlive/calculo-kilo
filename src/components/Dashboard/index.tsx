97a773fd2133ce8dc7ba97cebc726c2c8af01fae
Rafael <rafaelribeiro12@live.com>
2022-03-28 10:09:30 -0300

    Logic is working

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -5,27 +5,27 @@ export function Dashboard()
 
     const [vA, setVA] = useState('');
     const [vB, setVB] = useState('');
-    var vC;
+    let [vC, setVC] = useState(0)
 
     const handleInput = () =>
     {
-
-        vC = parseInt(vA) * parseInt(vB)
-        console.log(vC);
+        vC = parseFloat(vA) * parseFloat(vB);
+        setVC(vC)
     }
 
 
     return (
         <div>
             <h2>Valor do kilo</h2>
-            <input type='number' data-type='currency' placeholder="valor 1" onChange={ e => setVA(e.target.value) } />
+            <input type='number' step="0.01" placeholder="valor 1" onChange={ e => setVA(e.target.value) } />
 
             <h2>Quantidade do kilo comprada</h2>
-            <input type='number' placeholder="valor 1" onChange={ e => setVB(e.target.value) } />
+            <input type='number' step="0.01" placeholder="valor 1" onChange={ e => setVB(e.target.value) } />
 
             <button onClick={ () => handleInput() }>Calcular</button>
 
-            <h2>{ vC }</h2>
+
+            <h1>{ vB } kilos de { vA } reais desse produto custa { vC } reais.</h1>
 
         </div>
     );
