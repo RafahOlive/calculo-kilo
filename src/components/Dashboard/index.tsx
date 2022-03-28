c3aa6e0933400a91e19f85ea85d93f73973c8d66
Rafael <rafaelribeiro12@live.com>
2022-03-28 10:32:32 -0300

    alter dashboard text

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -17,15 +17,15 @@ export function Dashboard()
     return (
         <div>
             <h2>Valor do kilo</h2>
-            <input type='number' step="0.01" placeholder="valor 1" onChange={ e => setVA(e.target.value) } />
+            <input type='number' step="0.01" onChange={ e => setVA(e.target.value) } />
 
             <h2>Quantidade do kilo comprada</h2>
-            <input type='number' step="0.01" placeholder="valor 1" onChange={ e => setVB(e.target.value) } />
+            <input type='number' step="0.01" onChange={ e => setVB(e.target.value) } />
 
             <button onClick={ () => handleInput() }>Calcular</button>
 
 
-            <h1>{ vB } kilos de { vA } reais desse produto custa { vC } reais.</h1>
+            <h1>Preço do kilo: { vC }</h1>
 
         </div>
     );
